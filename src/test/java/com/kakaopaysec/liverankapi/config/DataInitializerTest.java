4b0608372b05c548fe3efad5996175bc952c07d7
roehfl <roehfl@gmail.com>
2024-05-28 22:09:03 +0900

    변경

--- src/test/java/com/kakaopaysec/liverankapi/config/DataInitializerTest.java
+++ src/test/java/com/kakaopaysec/liverankapi/config/DataInitializerTest.java
@@ -1,27 +1,38 @@
 package com.kakaopaysec.liverankapi.config;
 
 import com.kakaopaysec.liverankapi.common.CommonUtils;
+import com.kakaopaysec.liverankapi.domain.repository.StockDetailsRepository;
 import com.kakaopaysec.liverankapi.domain.repository.StockItemsRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class DataInitializerTest {
-    StockItemsRepository stockItemsRepository;
-    CommonUtils commonUtils;
-    DataInitializer dataInitializer;
+    @Test
+    void dataInitializerConstructorTest() {
+        StockItemsRepository stockItemsRepository = mock(StockItemsRepository.class);
+        CommonUtils commonUtils = mock(CommonUtils.class);
 
-    @BeforeEach
-    public void setUp() {
-        stockItemsRepository = mock(StockItemsRepository.class);
-        mock(CommonUtils.class);
-        new DataInitializer(stockItemsRepository, commonUtils);
+        DataInitializer dataInitializer = new DataInitializer(stockItemsRepository, commonUtils);
+
+        assertNotNull(dataInitializer, "DataInitializer should not be null");
     }
 
     @Test
-    void dataInitializerConstructorTest() {
-        assertNotNull(dataInitializer, "DataInitializer should not be null");
+    void loadDataTest() {
+        // 이건 내일 무슨의민지 확인좀 해보자..
+        StockItemsRepository stockItemsRepository = mock(StockItemsRepository.class);
+        CommonUtils commonUtils = mock(CommonUtils.class);
+
+        DataInitializer dataInitializer = new DataInitializer(stockItemsRepository, commonUtils);
+
+        // Call the method under test
+        dataInitializer.loadData();
+        // Verify that the method call was as expected
+        verify(stockItemsRepository).saveAll(anyList());
     }
 }
